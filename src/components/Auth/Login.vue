f69a12ecbb417bc4c7e7a3d174d46cc59fbc19fc
Andoogle15 <penzov.r.i.20@gmail.com>
2021-10-22 12:29:33 +0300

    Пункт 3.3 Валидация полей ввода

--- src/components/Auth/Login.vue
+++ src/components/Auth/Login.vue
@@ -5,20 +5,23 @@
 			<v-card class="elevetion-12">
 				<v-toolbar dark color="primary">Login</v-toolbar>
 				<v-card-text>
-					<v-form>
+					<v-form v-model="valid" ref="form"
+					validation>
 					<v-text-field
 						prepend-icon="mdi-account" 							
 						name="email" 
 						label="Email" 
 						type="email"
-						v-model="email">
+						v-model="email"
+						:rules="emailRules">
 					</v-text-field>
 					<v-text-field
 						prepend-icon="mdi-lock" 
 						name="password" 
 						label="Password" 
 						type="password"
-						v-model="password">
+						v-model="password"
+						:rules="passwordRules">
 					</v-text-field>
 					</v-form> 
 				</v-card-text>
@@ -36,7 +39,16 @@ export default{
 	data(){
 		return{
 			email: "",
-			password: ""
+			password: "",
+			valid: false,
+			emailRules: [
+				v => !!v || 'E-mail is required',
+				v => /.+@.+\..+/.test(v) || 'E-mail must be valid'
+			],
+			passwordRules: [
+				v => !!v || 'Name is required',
+				v => (v && v.length >= 6) || 'Password must be more or equel than 6 characters',
+			]
 		}
 	}
 }
