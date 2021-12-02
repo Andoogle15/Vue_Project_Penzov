cf1d80d16fd0e5618c82c224468b15ca087c9739
Andoogle15 <penzov.r.i.20@gmail.com>
2021-12-02 20:02:22 +0300

    Пункт 5.5 Подключение данных к Grid

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,7 +9,8 @@
            :key="ad.id"
            :src="ad.src">
            <div class="ad-link">
-               <v-btn class="error":to="'/ad/' + ad.id">
+               <v-btn class="error"
+               :to="'/ad/' + ad.id">
               {{ ad.title }}
                </v-btn>
                </div>
@@ -27,7 +28,7 @@
           ></v-img>
           <v-card-title primary-title>
           <div>
-            <h3 class="headline mb0">Здесь будет заголовок</h3>
+            <h3 class="headline mb-0">Здесь будет заголовок</h3>
             <div>Здесь будет описание</div>
           </div>
           </v-card-title>
@@ -42,6 +43,31 @@
       </v-flex>
     </v-layout> 
   </v-container>
+  <v-container grid-list-lg>
+     <v-layout row wrap>
+         <v-flex xs12 sm6 md4
+         v-for="ad in ads"
+         :key="ad.id">
+         <v-card>
+            <v-img
+            :src="ad.src"
+            height="200px"></v-img>
+            <v-card-title primary-title>
+                <div>
+                    <h3 class="headline mb-0">{{ad.title}}</h3>
+                    <div>{{ad.desc}}</div>
+                </div>
+            </v-card-title>
+            <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn text :to="'/ad/' + ad.id">   
+            Buy
+            </v-btn>
+            </v-card-actions>
+         </v-card>
+         </v-flex>
+     </v-layout>
+  </v-container>
   </div>
 </template>
 
@@ -49,49 +75,49 @@
 export default {
   data () { 
     return {
-		ads: [
-			{
-				title:"First",
-				desc:"First Desc",
-				promo: true,
-				src:"https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",	
-				id:"1"
-			},
-			{
-				title:"Second",
-				desc:"Second Desc",
-				promo: true,
-				src:"https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
-				id:"2"
-			},
-			{
-				title:"Third",
-				desc:"Third Desc",
-				promo: true,
-				src:"https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
-				id:"3"
-			},
-			{
-				title:"Fouth",
-				desc:"Fouth Desc",
-				promo: true,
-				src:"https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
-				id:"4"
-			},
-		]
+        ads: [
+            {
+                title:"First",
+                desc:"First Desc",
+                promo: true,
+                src:"https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",	
+                id:"1"
+            },
+            {
+                title:"Second",
+                desc:"Second Desc",
+                promo: true,
+                src:"https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
+                id:"2"
+            },
+            {
+                title:"Third",
+                desc:"Third Desc",
+                promo: true,
+                src:"https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
+                id:"3"
+            },
+            {
+                title:"Fouth",
+                desc:"Fouth Desc",
+                promo: true,
+                src:"https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
+                id:"4"
+            },
+        ]
     }   
   }
 } 
 </script>
 <style scoped> 
-	.ad-link {
-		position:absolute;
-		bottom: 50px;
-		left: 50%;
-		background: rgb(0, 0, 0, 0.5);
-		transform: translate(-50%, 0);
-		padding: 5px, 15px;
-		border-top-right-radius: 5px;
-		border-top-left-radius: 5px;
-	}
+    .ad-link {
+        position:absolute;
+        bottom: 50px;
+        left: 50%;
+        background: rgb(0, 0, 0, 0.5);
+        transform: translate(-50%, 0);
+        padding: 5px, 15px;
+        border-top-right-radius: 5px;
+        border-top-left-radius: 5px;
+    }
 </style>
