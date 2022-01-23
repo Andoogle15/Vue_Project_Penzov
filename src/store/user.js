e680ba10e3c6a78298a531c16f7a6f4757094e14
Andoogle15 <penzov.r.i.20@gmail.com>
2022-01-23 20:27:42 +0300

    Пункт 14.2

--- src/store/user.js
+++ src/store/user.js
@@ -10,9 +10,15 @@ export default {
     },
     actions:{
         registerUser({commit},{email, password}){
+            commit('clearError')
+            commit('setLoading', true)
             fb.auth().createUserWithEmailAndPassword(email,password).then(response => {
-                console.log(response.user.uid)
                 commit('setUser', new User(response.user.uid))
+                commit('setLoading', false)
+            }).catch(error => {
+                commit('setLoading', false)
+                commit('setError', error.message)
+        throw error
             })
         }
     },
